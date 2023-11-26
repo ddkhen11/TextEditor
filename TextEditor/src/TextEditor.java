93909c77bd07e27c4436247b9edf7e6cac7d7ccf
ddkhen11 <d.d.khen@wustl.edu>
2023-11-25 19:31:24 -0500

    Added font spinner and text color changer button

--- TextEditor/src/TextEditor.java
+++ TextEditor/src/TextEditor.java
@@ -1,13 +1,20 @@
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JColorChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JScrollPane;
+import javax.swing.JSpinner;
 import javax.swing.JTextArea;
 import javax.swing.ScrollPaneConstants;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class TextEditor extends JFrame implements ActionListener{
 
@@ -15,6 +22,9 @@ public class TextEditor extends JFrame implements ActionListener{
 	
 	JTextArea textArea;
 	JScrollPane scrollPane;
+	JLabel fontLabel;
+	JSpinner fontSizeSpinner;
+	JButton textColorButton;
 	
 	TextEditor() {
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -32,13 +42,40 @@ public class TextEditor extends JFrame implements ActionListener{
 		scrollPane.setPreferredSize(new Dimension(450, 450));
 		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		
+		fontLabel = new JLabel("Font: ");
+		
+		fontSizeSpinner = new JSpinner();
+		fontSizeSpinner.setPreferredSize(new Dimension(50, 25));
+		fontSizeSpinner.setValue(20);
+		fontSizeSpinner.addChangeListener(new ChangeListener(){
+
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				textArea.setFont(new Font(textArea.getFont().getFamily(), Font.PLAIN, (int) fontSizeSpinner.getValue()));
+				
+			}
+			
+		});
+		
+		textColorButton = new JButton("Color");
+		textColorButton.addActionListener(this);
+		
+		
+		this.add(fontLabel);
+		this.add(fontSizeSpinner);
+		this.add(textColorButton);
 		this.add(scrollPane);
 		this.setVisible(true);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		if (e.getSource() == textColorButton) {
+			Color color = JColorChooser.showDialog(null, "Choose a color", Color.black);
+			textArea.setForeground(color);
+		}
+		
+		
 		
 	}
 
